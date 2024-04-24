1bd7f60e931572b9f5152e70c0b4b7d49c68c0c3
Moh1tsingh <thakurmohitsingh2003@gmail.com>
2024-04-24 20:33:31 +0530

    change nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,19 +5,28 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <div className=" w-full flex flex-col justify-between ">
-      <div className=" w-full p-2 flex justify-between items-center px-24">
-        <Image src={image} width={150} alt="Logo" />
-        <h1 className=" text-2xl font-bold text-neutral-600">
-            Contact us - xyz@email.com
-        </h1>
+      <div className=" w-full flex flex-row text-[#763504]  items-center px-8 py-2">
+        <div className=" basis-1/6 flex justify-center ">
+          <Image src={image} width={120} alt="Logo" />
+        </div>
+        <div className=" basis-5/6">
+          <h1 className=" text-3xl font-bold tracking-normal ">
+            Government Dental College
+          </h1>
+          <p>
+            Dental College and Hospital, Chhatrapati Sambhajinagar, Maharashtra
+          </p>
+        </div>
       </div>
-      <div className="w-full bg-[#d8cbc4] flex justify-end gap-6 text-lg font-semibold py-2 pr-24">
-        <Link href={"/"}>Home</Link>
-        <Link href={"/departments"}>Departments</Link>
-        <Link href={"/departments"}>Departments</Link>
-        <Link href={"/departments"}>Departments</Link>
-        <Link href={"/"}>Contact</Link>
-        <Link href={"/about"}>About us</Link>
+      <div>
+        <div className="w-full bg-[#FFF1CF] text-[#8C3504] flex justify-end gap-6 text-lg font-medium py-2 pr-24">
+          <Link href={"/"}>Home</Link>
+          <Link href={"/departments"}>Departments</Link>
+          <Link href={"/departments"}>Departments</Link>
+          <Link href={"/departments"}>Departments</Link>
+          <Link href={"/"}>Contact</Link>
+          <Link href={"/about"}>About us</Link>
+        </div>
       </div>
     </div>
   );
